79c150634b54a42f69080113b2d82ef696103a85
Chandan <chandancvp08@gmail.com>
2021-12-13 08:52:28 +0530

    tried reading and writing

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,36 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Gun from 'gun'
 
+
 function index() {
+  var gun = Gun(["http://localhost:3000/", "https://testing-gun.vercel.app/"])
+  var db = gun.get("test").get("rec")
+  const [messages, setmessages] = useState([])
+  const [text, settext] = useState("")
+    
+  useEffect(()=>{
+
+    db.on((data)=>{
+      console.log(data)
+      console.log(data.messages)
+      console.log(eval(data.messages))
+      setmessages(eval(data.messages))
+    })
+  },[])
+
   return (
-    <div>
-      <p>hello world</p>
+    <div className='flex  col-auto flex-col row-auto w-auto'>
+      <p className="bg-green-600 text-lg">hello world</p>
+      <input className='border border-2 w-3/4' onChange={(e)=> settext(e.target.value)} />
+        <button onClick={()=>{
+          db.put({
+          messages: (JSON.stringify([...messages,text]))
+        })}}>submit</button>
+       {messages.map((element)=>{
+         return <p>{element}</p>
+       })}
+    
+
     </div>
   )
 }
